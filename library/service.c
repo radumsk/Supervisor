d48270e4b8aec7f3ec6dcc20aa5d2566d97d6e00
Radu-Mihai Costache <raducostache@mail.ru>
2023-12-22 15:54:32 +0200

    service header

---
+++ library/service.c
@@ -0,0 +1,8 @@
+//
+// Created by radu on 12/22/23.
+//
+
+#include "service.h"
+
+
+
---
+++ library/service.h
@@ -0,0 +1,49 @@
+//
+// Created by radu on 12/22/23.
+//
+
+#ifndef SUPERVISORLIBRARY_SERVICE_H
+#define SUPERVISORLIBRARY_SERVICE_H
+
+#define SUPERVISOR_FLAGS_CREATESTOPPED 0x1
+#define SUPERVISOR_FLAGS_RESTARTTIMES(times) ((times & 0xF) << 16)
+#define SUPERVISOR_STATUS_RUNNING 0x1
+#define SUPERVISOR_STATUS_PENDING 0x2
+#define SUPERVISOR_STATUS_STOPPED 0x4
+
+
+#include "supervisor.h"
+
+typedef struct {
+    supervisor_t supervisor;
+    char *servicename;
+    //poate mai trebuie si alte informatii, momentan e suficient dpmdv
+} service_t;
+
+//cerinta 2
+service_t service_create(
+        supervisor_t supervisor,
+        const char * servicename,
+        const char * program_path,
+        const char ** argv,
+        int argc,
+        int flags
+);
+int service_close(service_t service);
+
+//cerinta 3
+service_t service_open(
+        supervisor_t supervisor,
+        const char * servicename
+);
+int service_status(service_t service);
+
+//cerina 4
+int service_suspend(service_t service);
+int service_resume(service_t service);
+
+//cerinta 5
+int service_cancel(service_t service);
+int service_remove(service_t service);
+
+#endif //SUPERVISORLIBRARY_SERVICE_H
