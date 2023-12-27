e34b7ba46a3408c312b8a9f1a394c3ba9c0ea170
david <berbece.david27@gmail.com>
2023-12-27 19:56:23 +0200

    niste functii comentate in service.c

--- library/service.c
+++ library/service.c
@@ -105,3 +105,157 @@ service_t service_open(supervisor_t supervisor, const char *servicename) {
 
     return service;
 }
+
+/*
+int service_suspend(service_t service){
+
+    if (send_command(service.supervisor, "suspend", 7, service.servicename, strlen(service.servicename))) {
+        perror("send_command");
+        return -1;
+    }
+
+    char *command;
+    ssize_t command_size;
+    void *params;
+    ssize_t params_size;
+
+    if (receive_command(service.supervisor, &command, &command_size, &params, &params_size)) {
+        perror("receive_command");
+        return -1;
+    }
+
+    if (strcmp(command, "ok") != 0) {
+        printf("Received unexpected command: %s\n", command);
+        return -1;
+    }
+
+    if (params_size == sizeof(int)) {
+        memcpy(&service.status, params, params_size);
+    } else {
+        printf("Received unexpected params size for service status\n");
+    }
+
+    kill(service.pid, SIGSTOP);
+    free(command);
+    free(params);
+
+    return 0;
+}
+
+int service_resume(service_t service){
+
+    if (send_command(service.supervisor, "resume", 6, service.servicename, strlen(service.servicename))) {
+        perror("send_command");
+        return -1;
+    }
+
+    char *command;
+    ssize_t command_size;
+    void *params;
+    ssize_t params_size;
+
+    if (receive_command(service.supervisor, &command, &command_size, &params, &params_size)) {
+        perror("receive_command");
+        return -1;
+    }
+
+    if (strcmp(command, "ok") != 0) {
+        printf("Received unexpected command: %s\n", command);
+        return -1;
+    }
+
+    if (params_size == sizeof(int)) {
+        memcpy(&service.status, params, params_size);
+    } else {
+        printf("Received unexpected params size for service status\n");
+    }
+
+    kill(service.pid, SIGCONT);
+    free(command);
+    free(params);
+
+    return 0;
+}
+
+int service_cancel(service_t service){
+    // opreste serviciul, in timp ce handlerul obtinut cu service_open() este inca valid
+    // (nu a fost inchis cu service_close() sau service_remove())
+
+    if (send_command(service.supervisor, "cancel", 6, service.servicename, strlen(service.servicename))) {
+        perror("send_command");
+        return -1;
+    }
+
+    char *command;
+    ssize_t command_size;
+    void *params;
+    ssize_t params_size;
+
+    if (receive_command(service.supervisor, &command, &command_size, &params, &params_size)) {
+        perror("receive_command");
+        return -1;
+    }
+
+    if (strcmp(command, "ok") != 0) {
+        printf("Received unexpected command: %s\n", command);
+        return -1;
+    }
+
+    if (params_size == sizeof(int)) {
+        memcpy(&service.status, params, params_size);
+    } else {
+        printf("Received unexpected params size for service status\n");
+    }
+
+    int result = kill(service.pid, SIGKILL);
+
+    if (result == -1) {
+        perror("kill");
+    }
+
+    free(command);
+    free(params);
+
+}
+
+int service_remove(service_t service){
+    // inchide handlerul obtinut cu service_open() si elibereaza resursele asociate
+
+    if (send_command(service.supervisor, "remove", 6, service.servicename, strlen(service.servicename))) {
+        perror("send_command");
+        return -1;
+    }
+
+    char *command;
+    ssize_t command_size;
+    void *params;
+    ssize_t params_size;
+
+    if (receive_command(service.supervisor, &command, &command_size, &params, &params_size)) {
+        perror("receive_command");
+        return -1;
+    }
+
+    if (strcmp(command, "ok") != 0) {
+        printf("Received unexpected command: %s\n", command);
+        return -1;
+    }
+
+    if (params_size == sizeof(int)) {
+        memcpy(&service.status, params, params_size);
+    } else {
+        printf("Received unexpected params size for service status\n");
+    }
+
+    free(command);
+    free(params);
+    free(service.servicename);
+    free(service.args);
+    service.supervisor = -1;
+    service.pid = -1;
+    service.status = -1;
+    service.restart_times = -1;
+    service.flags = -1;
+    service.service_id = -1;
+}
+*/
